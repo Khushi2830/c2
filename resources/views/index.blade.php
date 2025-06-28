1547f16887d3c7d03528bdaf4a003b4aef2b4f88
khushi2830 <khumarikhushi03@gmail.com>
2025-06-29 01:59:36 +0530

    Enhance product display: update HomeController to pass products to the index view; improve filtercategory and index blade templates for better layout and styling of product cards.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,26 +33,23 @@
 
 
   <div class="container p-0 mt-0">
-    <h2 class="section-title" style="color: #7b2dc3;">Cakes</h2>
+    <h2 class="section-title" style="color: #7b2dc3;">All Products</h2>
     <div class="row flex-nowrap overflow-auto g-3">
-
+  @foreach ($products as $product)
     <div class="col-10 col-sm-6 col-md-4 col-lg-3">
+     
       <div class="product-card">
       <div class="product-img">
-        <img src="https://d1f3aa6ifduais.cloudfront.net/assets/images/products/1674039776019_84.jpg" alt="Cake">
-        <div class="price-tag" style=" background-color: #7b2dc3;">₹550.00</div>
+       <img src="{{ asset("storage/" . $product->image) }}" class="card-img-top" alt="Cake 1">
+        <div class="price-tag" style=" background-color: #7b2dc3;"><del>₹{{$product->price}}</del>₹{{$product->descount_price}}</div>
       </div>
       <div class="product-details">
-        <h6>Exotic Butterscotch Cake</h6>
+        <h6>{{$product->title}}</h6>
         <p class="text-success mb-1">
         <img src="https://cdn-icons-png.flaticon.com/512/3480/3480301.png" class="veg-icon" alt=""> 100% Veg.
         </p>
         <div class="product-footer">
-        <div>
-          <div class="weight-box d-inline">0.5</div>
-          <span class="ms-1">KGS</span>
-        </div>
-        <button class="add-cart-btn border-0 " style=" background-color: #7b2dc3;">
+        <a class="add-cart-btn border-0 " href="{{ route("view", $product->id) }}" style=" background-color: #7b2dc3;">
           <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" stroke="white" stroke-width="2"
           stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"
           viewBox="0 0 24 24">
@@ -60,11 +57,13 @@
           <circle cx="20" cy="21" r="1" />
           <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6" />
           </svg>
-        </button>
+        </a>
         </div>
       </div>
       </div>
+      
     </div>
+    @endforeach
 
 
     </div>
