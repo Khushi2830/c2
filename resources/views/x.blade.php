6a2875c44d316198d09c12aa073439bbd055d4f3
khushi2830 <khumarikhushi03@gmail.com>
2025-06-30 13:04:29 +0530

    Remove search input from parent view and update cart icon display with item count in x.blade.php

--- resources/views/x.blade.php
+++ resources/views/x.blade.php
@@ -874,8 +874,11 @@
           </li>
         </ul>
       </div>
-      <a href="{{ route("show.cart") }}"><i class="fas fa-shopping-cart fs-5" style="color: #6f42c1;"></i></a>
-
+       <a href="{{ route("show.cart") }}"><i class="fas fa-shopping-cart fs-5"  style="color: #6f42c1;"></i> 
+      <sup>
+        {{ count(request()->session()->get('cart', [])) }}
+      </sup>
+      </a>
 
 
     </div>
