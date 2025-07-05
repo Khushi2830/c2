d7bb32c6cfe38f5b2db7bb0ce441f01d6f7b89e7
aastha058 <aasthapriya185@gmail.com>
2025-07-05 16:37:11 +0530

    style: update Bootstrap script references for consistency across pages

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -84,5 +84,6 @@
     @endforeach
   </div>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
 @endsection
--- resources/views/indexparent.blade.php
+++ resources/views/indexparent.blade.php
@@ -8,6 +8,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
   <style>
     .xx {
